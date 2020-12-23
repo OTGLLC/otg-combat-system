c393efc1164d315d82b2eacabd966054d9a425b7
Jason <jrespress@otgstudio.online>
2020-12-23 16:16:19 -0600

    Added Editor Folder

---
+++ Editor/Common/EditorConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace OTG.CombatSystem.Editor
+{
+   
+    [CreateAssetMenu]
+    public class EditorConfig : ScriptableObject
+    {
+        [SerializeField] private string m_editorConfigsPath;
+        public string EditorConfigsPath { get { return m_editorConfigsPath; } }
+        [SerializeField] private string m_combatActionsPath;
+        public string CombatActionsPath { get { return m_combatActionsPath; } }
+        [SerializeField] private string m_combatTransitionsPath;
+        public string CombatTransitionsPath { get { return m_combatTransitionsPath; } }
+        [SerializeField] private string m_characterPAthRoot;
+        public string CharacterPathRoot { get { return m_characterPAthRoot; } }
+        [SerializeField] private string m_templatesPath;
+        public string TemplatesPaths { get { return m_templatesPath; } }
+        [SerializeField] private string m_characterSavedGraphsPath;
+        public string CharacterSavedGraphsPath { get { return m_characterSavedGraphsPath; } }
+
+        //[SerializeField] private OTGGlobalCombatConfig m_globalCombatConfig;
+        //public OTGGlobalCombatConfig GlobalCombatConfig { get { return m_globalCombatConfig; } }
+        
+    }
+
+}
